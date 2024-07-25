d0d8ae60f5edea67fa50b1cbc9b998fb437b96f3
kasuich <kasui3108@gmail.com>
2024-07-26 00:32:04 +0300

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it --gpus all -v /home/kasuich/projects/summer_school_project/demo/summer_school_project:/workspace/ -v /home/kasuich/.cache/:/home/user/.cache/ -p 7007:7007 -p 8501:8501 --rm --ipc=host 3d-reconstruction
+docker run -it --gpus all -v summer_school_project:/workspace/ -v /home/<YOUR-USERNAME>/.cache/:/home/user/.cache/ -p 7007:7007 -p 8501:8501 --rm --ipc=host 3d-reconstruction
